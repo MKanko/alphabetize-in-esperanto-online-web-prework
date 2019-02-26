871d3038d8b9cfdf2ff7be19120ed3195b5d5ba9
Mark Kanko <mkanko66@gmail.com>
2019-02-26 20:14:35 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -6,3 +6,11 @@ def alphabetize(arr)
     end 
   end 
 end
+
+
+# Big Take away on the method above - I had already done reading
+# about sorting in arrays and understood how numbers and strings
+# and converting words in strings into ellemnts in array and how
+# hierarchy of letters in alphabet are compared. So I had a vague 
+# idea of direction to solve this. I did the reading of the provided
+# resources
